71d0e040cb142eba276eb028be696f1ca4828d38
John Stockbauer <john@shippable.com>
2017-07-28 16:46:40 -0700

    simplify

--- runBuilds.sh
+++ runBuilds.sh
@@ -7,15 +7,13 @@ for i in $(seq 1 16);
 do
   NAME=$DIR_PREFIX$i
   echo "name is: $NAME"
-  pushd svc1
-  docker build -t trriplejay/$NAME:latest . &
+  docker build --no-cache -t trriplejay/$NAME:latest . &
   PIDS[${i}]=$!
-  popd
 done
 
 for pid in ${PIDS[*]};
 do
   echo "waiting on process $pid"
   wait $pid
-  echo "Prcoess $pid exited with status $?"
+  echo "Process $pid exited with status $?"
 done
